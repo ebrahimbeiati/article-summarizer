8f46864eb5155c7b401ac9d69663c167f5e20e37
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-09-28 18:38:46 +0100

    finish hero part

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import {logo} from '../assets'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex-col items-center justify-center'>
+    <nav className='flex justify-between items-center w-full mb-10 pt-4'>
+    <img src={logo} alt="sumz_logo" className='w-25 object-contain' />
+    <button
+    type='button'
+    onChange={()=>window.open('https://gist.github.com/ebrahimbeiati')}
+    className='bg-blue-500 hover:bg-black text-white font-bold py-2 px-4 rounded'
+    >
+    My GitHub
+
+    </button>
+
+    </nav>
+    <h1 className='head_text'>
+    Article Summarizer with
+    <br className='max-md:hidden' /> <span className='orange_gradient'>OpenAI</span>
+    </h1>
+    <h2 className="head_text1">
+    Summarize your articles with this amazing OpenAI article summarizer
+    </h2>
+
+    </header>
   )
 }
 
