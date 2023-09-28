f300521e61e2528d09c74624e85f579a8fd00690
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2023-09-28 21:19:42 +0100

    Update codebase to use Rapid API for article summarization

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,29 +1,42 @@
 import { useState,useEffect } from "react"
 import { copy, linkIcon, loader, tick } from "../assets"
+import { useLazyGetArticleQuery } from "../services/article"
+
 
 const Demo = () => {
   const [article, setArticle] = useState({
-   
     url: "",
     summary: "",
      title: "",
     description: "",
   });
-  const handleSubmit = async ()=>{
-    const response = await fetch("http://localhost:5000/summarize", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ url: article.url }),
-    });
-    const data = await response.json();
-    setArticle({
-      ...article,
-      summary: data.summary,
-      title: data.title,
-      description: data.description,
-    });
+  const [allArticles, setArticles] = useState([]);
+
+  const [getSummary, {error, isFetching}]= useLazyGetArticleQuery();
+  
+  useEffect(()=>{
+    
+    const articleFromLocalStorage = JSON.parse(
+      localStorage.getItem("article")
+    )
+    if(articleFromLocalStorage){
+      setArticle(articleFromLocalStorage)
+    }
+  },[]);
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    const {data} = await getSummary({article: article.url})
+    if(data?.summary){
+      const newArticle = {
+        ...article, summary: data.summary
+      }
+      const updatedAllArticles=[newArticle,...allArticles];
+      setArticle(newArticle)
+      setArticles(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles))
+    }
+
   }
 
 
@@ -63,8 +76,57 @@ const Demo = () => {
           </button>
         </form>
         {/*Search history */}
-
+        <div className="flex flex-col items-center justify-center gap-2 w-full">
+          <h2 className="text-center text-gray-700 text-xl font-bold">
+            Search History
+          </h2>
+          <div className="flex flex-col items-center justify-center gap-2 w-full overflow-y-auto">
+            {allArticles.map((item, index) => {
+                <div
+                  key={`link-${index}`}
+                  onClick={()=>setArticle(item)}
+                  className="link_card"
+                >
+                <div className="copy_btn">
+                  <img src={copy} alt="copy_icon"
+                  className="cursor-pointer"/>
+                </div>
+                <p
+                className="text-sm text-gray-600">
+                  {item.url}
+                </p>
       </div>
+            })}
+          </div>
+        </div>
+      </div>
+      <div
+      className="my-10 max-w-full flex justify-center items-center">
+      {isFetching ?(
+        <img src={loader} alt="loader" className="w-20 h-20 object-contain"/>
+      ): error? (
+        <p className="font-inter text-black text-center">
+          Sorry
+          <span >
+            {error?.data?.error}
+          </span>
+        </p>
+      ):(
+        article.summary && (
+          <div className="flex flex-col gap-3">
+            <h2>
+              Article <span className="blue_gradient">Summary</span>
+            </h2>
+            <div className="summary_box">
+              <p>{article.summary}</p>
+            </div>
+          </div>
+        )
+
+     ) }
+      </div>
+        
+
           {/*Display results*/}
 
     </section>
--- src/services/article.js
+++ src/services/article.js
@@ -1,8 +1,26 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-
+const rapidApi = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
-    reducerPath: 'articleApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5000/summarize'}),
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+    headers.set("X-RapidAPI-Key",rapidApi);
+    headers.set("X-RapidAPI-Host", "article-extractor-and-summarizer.p.rapidapi.com");
+    return headers;
+    }
+  }),
+  
+  endpoints: (builder) => ({
+    getArticle: builder.query({
+      getSummary: builder.query({
+        query: (params) => `/summarize?url=$
+         {encodeURIComponent (params.articleUrl)}&length=4 `
+      })
+    })
 })
+})
+     
+export const { useLazyGetArticleQuery } = articleApi;
